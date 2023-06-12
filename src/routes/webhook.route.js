0be774eeda426947596ce2d69d6591f49ec38e93
RedRe4per <s3582474@gmail.com>
2023-06-12 10:14:06 +1000

    feat: init

--- src/routes/webhook.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { 
-    webhookHandler
-} = require('../controllers/webhook.controller');
-
-const webhookRouter = express.Router();
-webhookRouter.post('', webhookHandler);
-
-module.exports = webhookRouter;
