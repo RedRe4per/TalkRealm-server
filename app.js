0c83fa70c44f745813296f67762a9366ef36afb9
RedRe4per <s3582474@gmail.com>
2023-07-02 18:50:46 +1000

    feat: restructure socket and update userobj

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const errorHandler = require('./src/middleware/errorHandler');
+const socketSetup = require('./src/utils/socket');
 const app = express();
 require('express-async-errors');
 const router = require('./src/routes');
 
 const http = require('http');
-const { Server } = require('socket.io');
 
 app.use(helmet());
 app.use(cors());
@@ -18,60 +18,6 @@ app.use(errorHandler);
 app.use(`/${process.env.ENVIRONMENT}`, router);
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
-
-let users = [];
-
-io.on('connection', (socket) => {
-  console.log('User connected');
-
-  socket.on('message', (message) => {
-    console.log('hello');
-    io.emit('message', 'message');
-  });
-
-  // socket.on('join-room', (userId) => {
-  //   console.log("first11111")
-  //   //socket.join(roomId);
-  //   socket.broadcast.emit('user-connected', userId) //to(roomId)
-  // })
-
-  socket.on('I-connected', (userId) => {
-    console.log('User connected', userId);
-    users.push(userId);
-    //socket.join(roomId2);
-    socket.broadcast.emit('user-connected', { userId: userId, users: users }); //to(roomId)
-    socket.emit('user-connected', { userId: userId, users: users });
-
-    socket.on('disconnect', () => {
-      console.log('User disconnected', userId);
-      const index = users.indexOf(userId);
-      if (index > -1) {
-        users.splice(index, 1);
-      }
-      socket.broadcast.emit('user-disconnected', {
-        userId: userId,
-        users: users,
-      });
-    });
-  });
-
-  socket.on('I-disconnect', (userId) => {
-    console.log('User disconnected', userId);
-    const index = users.indexOf(userId);
-    if (index > -1) {
-      users.splice(index, 1);
-    }
-    socket.broadcast.emit('user-disconnected', {
-      userId: userId,
-      users: users,
-    });
-  });
-});
+socketSetup(server);
 
 module.exports = server;
---
+++ src/utils/socket.js
@@ -0,0 +1,64 @@
+const { Server } = require('socket.io');
+
+let users = [];
+
+module.exports = function (server) {
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('message', (message) => {
+      console.log('hello');
+      io.emit('message', 'message');
+    });
+
+    // socket.on('join-room', (userId) => {
+    //   console.log("first11111")
+    //   //socket.join(roomId);
+    //   socket.broadcast.emit('user-connected', userId) //to(roomId)
+    // })
+
+    socket.on('I-connected', (userObj) => {
+      console.log('User connected', userObj.userPeerId);
+      users.push(userObj);
+      //socket.join(roomId2);
+      socket.broadcast.emit('user-connected', {
+        userObj: userObj,
+        users: users,
+      }); //to(roomId)
+      socket.emit('user-connected', { userObj: userObj, users: users });
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected', userObj);
+        const index = users.findIndex(
+          (user) => user.userPeerId === userObj.userPeerId,
+        );
+        if (index > -1) {
+          users.splice(index, 1);
+        }
+        socket.broadcast.emit('user-disconnected', {
+          userObj: userObj,
+          users: users,
+        });
+      });
+    });
+
+    socket.on('I-disconnect', (userId) => {
+      console.log('User disconnected', userId);
+      const index = users.findIndex((user) => user.userPeerId === userId);
+      if (index > -1) {
+        users.splice(index, 1);
+      }
+      socket.broadcast.emit('user-disconnected', {
+        userId: userId,
+        users: users,
+      });
+    });
+  });
+};
